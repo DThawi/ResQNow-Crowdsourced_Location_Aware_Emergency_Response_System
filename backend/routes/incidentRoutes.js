dbb976275784e3d258056a784e19fc190f727bb6
ChanindiRanasinghe <chanindiranasinghe@gmail.com>
2026-04-02 09:28:17 +0530

    Move two files into admin folder

--- backend/routes/incidentRoutes.js
+++ backend/routes/incidentRoutes.js
@@ -3,15 +3,17 @@ const router = express.Router();
 const incidentController = require('../controllers/incidentController');
 const { verifyToken } = require('../middleware/authMiddleware');
 const allowRoles = require("../middleware/roleMiddleware"); // <-- ADD THIS
-const upload = require('../middleware/uploadMiddleware'); 
+const { upload, uploadToCloudinary } = require("../middleware/upload"); 
 
 router.get('/', incidentController.getAllIncidents);
 
 router.post(
-  '/',
-  verifyToken,
-  upload.single("image"),   // only accepts 'image'
-  incidentController.createIncident
+  "/",
+  upload.single("image"),
+  uploadToCloudinary,
+  async (req, res) => {
+    // your logic here
+  }
 );
 
 router.put('/:id/status', verifyToken, allowRoles("Admin"), incidentController.updateIncidentStatus);
