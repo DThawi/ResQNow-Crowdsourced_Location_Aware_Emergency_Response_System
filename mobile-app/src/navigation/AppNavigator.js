495bacaed586ba34cc8e3993b2bc9137c57cd6b4
KavinduAnj <wkanjana1590@gmail.com>
2026-04-07 10:47:37 +0530

    HomeScreen with incident data fetching logic

--- mobile-app/src/navigation/AppNavigator.js
+++ mobile-app/src/navigation/AppNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import HomeHeader from "../components/HomeHeader";
 
 import LoginScreen from "../screens/auth/LoginScreen";
 import RegisterScreen from "../screens/auth/Register1"; // Assuming Register1 is the first step of registration
@@ -18,7 +18,13 @@ export default function AppNavigator() {
         {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
         {/* <Stack.Screen name="Register" component={RegisterScreen} /> */}
         {/* <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicy} /> */}
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            header: () => <HomeHeader />
+          }}
+        />
 
       </Stack.Navigator>
     </NavigationContainer>
